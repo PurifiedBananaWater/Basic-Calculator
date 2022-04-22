cc6e840bc374b03dbe5de1e9894da5a1d89e6a63
PurifiedBananaWater <103452203+PurifiedBananaWater@users.noreply.github.com>
2022-04-22 17:17:44 -0400

    Update main.cpp
    Got rid of meaningless comments.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 #include<string>
 #include<algorithm>
 #include<vector>
-//#include"interpreter.h"
 #include"basicmath.h"
-//#include"algebraone.h"
+
 
 
 using std::cout;
@@ -13,31 +12,9 @@ using std::string;
 
 
 int main() {
-	//Interpreter::Interpreter inter;
+
 	BasicMath::BasicMath basic;
 	string equation;
-
-	/*
-	std::vector<char> v2{'2', '4','/','4'};
-	std::string c;
-	std::string nums1, nums2;
-
-	int i = 0;
-	while (v2[i] != '/') {
-		c = (1, v2[i]);
-		nums1 += c;
-		std::cout << nums1 << "\n";
-		i++;
-	}
-	i++;
-	while (i != v2.size()) {
-		c = (1, v2[i]);
-		nums2 += c;
-		std::cout << nums2 << "\n";
-		i++;
-	}
-	cout << "Answer = " << stod(nums1) / stod(nums2);
-	*/
 	
 	while (equation != "Exit") {
 		cout << "Calculator enter equation below:\n";
